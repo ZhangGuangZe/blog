c2dd0ca1410de766bdf5c0d84e62e29857e3f3bf
ZhangGuangZe <zhanggz1997@126.com>
2020-04-12 22:10:52 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,24 +6,16 @@ module.exports = {
 
   // 元信息
   title: '前端男孩',
-  description: '让前端变得更简单',
+  description: '让学习前端变得更简单',
 
   // 配置主题
   themeConfig: {
     // 配置导航栏
     nav: [
       { text: '首页', link: '/' },
-      { text: '前端', link: '/front/' },
-      { text: '后端', link: '/back/' },
-      { text: '阅读', link: '/read/' },
-      { 
-        text: '语言', 
-        ariaLabel: 'Language Menu',
-        items: [
-          { text: 'Chinese', link: '/language/chinese' },
-          { text: 'English', link: '/language/english' }
-        ]
-      },
+      { text: '计算机基础', link: '/computer/' },
+      { text: '前端基础', link: '/front/' },
+      { text: '学习总结', link: '/summarize/front' },
       { text: 'GitHub', link: 'https://github.com/HotZhang' }
     ],
 
@@ -31,14 +23,25 @@ module.exports = {
     sidebar: {
       '/front/': [
         {
-          title: '前端',
+          title: '前端体系',
           collapsable: false,
           sidebarDepth: 1,
           children: [
+            [ '/front/', '简介' ],
             [ '/front/html', 'HTML' ],
             [ '/front/css', 'CSS' ],
-            [ '/front/javascript', 'JavaScript' ],
-            [ '/front/vue', 'Vue' ],
+            [ '/front/javascript', 'JavaScript' ]
+          ]
+        }
+      ],
+      '/summarize/': [
+        {
+          title: '学习总结',
+          collapsable: false,
+          sidebarDepth: 1,
+          children: [
+            [ '/summarize/front', '前端训练营总结' ],
+            [ '/summarize/algorithm', '数据结构与算法总结' ]
           ]
         }
       ]
@@ -51,7 +54,7 @@ module.exports = {
     },
 
     // 最后更新时间
-    lastUpdated: 'Last Updated',
+    lastUpdated: '更新时间',
 
     // 启动页面滚动效果
     smoothScroll: true,
--- docs/README.md
+++ docs/README.md
@@ -2,8 +2,8 @@
 home: true
 heroImage: /avatar.png
 heroText: 前端男孩
-tagline: 让前端变得更简单
-actionText: 快速上手 →
+tagline: 让学习前端变得更简单
+actionText: 开始学习之旅
 actionLink: /front/
 footer: MIT Licensed | Copyright © 2020-ZhangGuangZe
 ---
